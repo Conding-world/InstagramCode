90a8ca0b588c68f4ce937f8b7193f1740aff8c67
Tech-o-code <91176319+Tech-o-code@users.noreply.github.com>
2021-10-05 21:15:33 +0530

    Update security.php

--- security.php
+++ security.php
@@ -10,8 +10,26 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 
 ?>
+<!DOCTYPE html>
 
-<!DOCTYPE html>
+<html>
+
+<body>
+
+<?php
+
+ECHO "Hello World!<br>";
+
+echo "Hello World!<br>";
+
+EcHo "Hello World!<br>";
+
+?>
+
+</body>
+
+</html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
   <title>gaming YouTubers insta group</title>
